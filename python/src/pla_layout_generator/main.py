3bf8c5a1dd8367df81a5ce12b842b88a20a5e9d2
vinnyputty <vinayputreddi@gmail.com>
2019-05-28 16:50:24 -0700

    Update ordering in main module code

--- python/src/pla_layout_generator/main.py
+++ python/src/pla_layout_generator/main.py
@@ -1269,16 +1269,23 @@ def generate_pla_layout(write_to_file=False, input_file=None):
 
 
 if __name__ == '__main__':
+    generate_pla_layout(False)
+
     # load_definitions()
-    # load_pla_codes(True)
-    generate_pla_layout(True)
+    # load_pla_codes(False)
+
     # generate_pla_layout(True, "../../pla_codes/simple_pla_codes.pla")
     # generate_pla_layout(True, "../../pla_codes/4x4x16_pla_codes.pla")
+    # generate_pla_layout(True, "../../pla_codes/5x32x256_with_duplicate_codes_pla_codes.pla")
     # generate_pla_layout(True, "../../pla_codes/31x31x31_pla_codes.pla")
     # generate_pla_layout(True, "../../pla_codes/32x32x32_pla_codes.pla")
-    wrapped = wrapper(generate_pla_layout, True, "../../pla_codes/1024x1024x1024_pla_codes.pla")
-    print(timeit.timeit(wrapped, number=1))  # this takes some time on redstone
+    # generate_pla_layout(True, "../../pla_codes/32x32x256_pla_codes.pla")
+    # generate_pla_layout(True, "../../pla_codes/1024x1024x1024_pla_codes.pla")  # this takes some time on redstone
+    # wrapped = wrapper(generate_pla_layout, True, "../../pla_codes/1024x1024x1024_pla_codes.pla")  # for benchmarking
+    # print(timeit.timeit(wrapped, number=1) / 1)  # for benchmarking
+    # timeit_(generate_pla_layout) # doesn't work
     # generate_pla_layout(True, "../../pla_codes/1024x1024x4096_pla_codes.pla")  # this takes a long time on redstone
     # generate_pla_layout(True, "../../pla_codes/4096x4096x4096_pla_codes.pla")  # this takes forever on redstone; it
     # also requires about 6 GB of available memory
 
+    # generate_pla_wires_layout_code_key() # NotImplementedException or NotImplementedWarning
