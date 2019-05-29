3f4497c0ac28aff30d9dc03249460ad68e4e835f
Vinay Putreddi <root@vinayputreddi.com>
2019-05-28 19:52:08 -0700

    Reorder and group main module code in sections

--- python/src/pla_layout_generator/main.py
+++ python/src/pla_layout_generator/main.py
@@ -1281,11 +1281,12 @@ if __name__ == '__main__':
     # generate_pla_layout(True, "../../pla_codes/32x32x32_pla_codes.pla")
     # generate_pla_layout(True, "../../pla_codes/32x32x256_pla_codes.pla")
     # generate_pla_layout(True, "../../pla_codes/1024x1024x1024_pla_codes.pla")  # this takes some time on redstone
-    # wrapped = wrapper(generate_pla_layout, True, "../../pla_codes/1024x1024x1024_pla_codes.pla")  # for benchmarking
-    # print(timeit.timeit(wrapped, number=1) / 1)  # for benchmarking
-    # timeit_(generate_pla_layout) # doesn't work
     # generate_pla_layout(True, "../../pla_codes/1024x1024x4096_pla_codes.pla")  # this takes a long time on redstone
     # generate_pla_layout(True, "../../pla_codes/4096x4096x4096_pla_codes.pla")  # this takes forever on redstone; it
     # also requires about 6 GB of available memory
+
+    # wrapped = wrapper(generate_pla_layout, True, "../../pla_codes/1024x1024x1024_pla_codes.pla")  # for benchmarking
+    # print(timeit.timeit(wrapped, number=1) / 1)  # for benchmarking
+    # timeit_(generate_pla_layout) # doesn't work
 
     # generate_pla_wires_layout_code_key() # NotImplementedException or NotImplementedWarning
